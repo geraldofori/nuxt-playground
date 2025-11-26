1fe2858cc2430fd6fe024c975833755085f5e047
geraldofori <oforige@gmail.com>
2025-11-26 17:25:46 +0000

    chore: fixed issues with auto imports

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,10 @@
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from "@tailwindcss/vite"
 
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  alias: {
-    "@": resolve(__dirname, "/"),
-  },
   css: ["@/assets/css/main.css"],
   vite: {
-    plugins: [tailwindcss() as any],
+    plugins: [tailwindcss()],
   },
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
-});
+})
